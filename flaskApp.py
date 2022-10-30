e45f8ac2e5f582cae231d030911351165659438a
Rajtestgit <103324356+Rajtestgit@users.noreply.github.com>
2022-10-29 22:17:52 -0700

    Update flaskApp.py

--- flaskApp.py
+++ flaskApp.py
@@ -29,4 +29,4 @@ def home():
 	return render_template('home.html', free_time=free_time)
 
 if __name__ == '__main__':
-	app.run(host='172.31.0.227', debug=True)
+	app.run(host='0.0.0.0', debug=True)
